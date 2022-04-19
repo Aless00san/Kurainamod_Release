49442a867d62f0e8d7e1c1d31b2cdb760ebe4873
Aless00san <mumaitronzx@gmail.com>
2022-04-19 21:29:45 +0200

    The flight update

--- src/main/java/net/fabricmc/kurainamod/entity/KurainaEntity.java
+++ src/main/java/net/fabricmc/kurainamod/entity/KurainaEntity.java
@@ -1,17 +1,24 @@
 package net.fabricmc.kurainamod.entity;
 
+import com.google.common.collect.Sets;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.control.FlightMoveControl;
 import net.minecraft.entity.ai.goal.LookAroundGoal;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
+import net.minecraft.entity.ai.pathing.BirdNavigation;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.network.Packet;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
@@ -27,19 +34,24 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
+import java.util.Set;
+
 public class KurainaEntity extends TameableEntity implements IAnimatable, IAnimationTickable {
+    private static final Set<Item> TAMING_INGREDIENTS = Sets.newHashSet(Items.WHEAT_SEEDS);
 
     private AnimationFactory factory = new AnimationFactory(this);
 
     public KurainaEntity(EntityType<? extends TameableEntity> entityType, World world) {
         super(entityType, world);
+        this.moveControl = new FlightMoveControl(this, 10, false);
     }
 
     public static DefaultAttributeContainer.Builder createMobAttributes() {
         return LivingEntity.createLivingAttributes().add(EntityAttributes.GENERIC_FOLLOW_RANGE, 25.0D)
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 10)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 0.0D).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25D)
-                .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 1.0D);
+                .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 1.0D)
+                .add(EntityAttributes.GENERIC_FLYING_SPEED, 0.4f);
     }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
@@ -47,8 +59,16 @@ public class KurainaEntity extends TameableEntity implements IAnimatable, IAnima
             event.getController().setAnimation(new AnimationBuilder().addAnimation("Fly", true));
             return PlayState.CONTINUE;
         }
-        event.getController().setAnimation(new AnimationBuilder().addAnimation("Walk", true));
+        if(event.isMoving() && this.isOnGround()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("Walk", true));
+            return PlayState.CONTINUE;
+        }
+        event.getController().setAnimation(new AnimationBuilder().addAnimation(("Idle1"), false));
         return PlayState.CONTINUE;
+    }
+
+    public boolean handleFallDamage(float fallDistance, float damageMultiplier, DamageSource damageSource) {
+        return false;
     }
 
     @Override
@@ -86,4 +106,12 @@ public class KurainaEntity extends TameableEntity implements IAnimatable, IAnima
         return ClientListener.EntityPacket.createPacket(this);
     }
 
+    @Override
+    protected EntityNavigation createNavigation(World world) {
+        BirdNavigation birdNavigation = new BirdNavigation(this, world);
+        birdNavigation.setCanPathThroughDoors(false);
+        birdNavigation.setCanSwim(true);
+        birdNavigation.setCanEnterOpenDoors(true);
+        return birdNavigation;
+    }
 }
